c0527501f789340233c27db90f5d7ee6eca5ec0f
hafif07 <muhammadhafifalbusyro@gmail.com>
2020-08-01 10:41:06 +0700

    pretty style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import MainNavigation from './src/navigation/MainNavigation';
+import MainNavigation from './navigation/MainNavigation';
 
 class App extends React.Component {
   render() {
