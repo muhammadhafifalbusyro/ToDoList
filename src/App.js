10fa3883c79b9e8390e9013f615f75d162b2fa2e
Muhammad Hafif Al Busyro <41612593+muhammadhafifalbusyro@users.noreply.github.com>
2020-08-10 21:10:18 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import MainNavigation from './navigation/MainNavigation';
-
+//Make class
 class App extends React.Component {
   render() {
     return <MainNavigation />;
